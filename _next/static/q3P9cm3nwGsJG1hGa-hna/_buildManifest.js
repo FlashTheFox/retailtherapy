808eeeffaeae5a5e4bd37de29fbea989dacff31d
Booker <bookerjmartin@gmail.com>
2022-04-26 15:22:16 -0700

    Updates

---
+++ _next/static/q3P9cm3nwGsJG1hGa-hna/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/95b64a6e-9562e008bb67a5b8.js","static/chunks/d7eeaac4-63cca84de534d71f.js","static/chunks/pages/index-587906fab1173c30.js"],"/_error":["static/chunks/pages/_error-2f883067a14f4c4a.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
